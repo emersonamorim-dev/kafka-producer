f520656a41f3ce198bda97776f9505cb7d8b5edf
Emerson Rockman <emerson_tecno@hotmail.com>
2022-07-12 09:38:28 -0300

    Kafka-Producer

---
+++ src/main/java/com/codev/kafka/producer/resource/MensagemResource.java
@@ -0,0 +1,24 @@
+package com.codev.kafka.producer.resource;
+
+import com.codev.kafka.producer.service.MensagemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/mensagem")
+public class MensagemResource {
+
+    @Autowired
+    MensagemService mensagemService;
+
+    @PostMapping
+    public ResponseEntity<String> enviarMensagem(@RequestBody String mensagem) {
+        mensagemService.sendMessage(mensagem);
+        return ResponseEntity.ok().body("Mensagem enviada com sucesso: " + mensagem);
+    }
+
+}
